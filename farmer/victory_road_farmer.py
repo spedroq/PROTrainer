68d313381feb539308f6322350476bae29d46b7e
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2018-03-25 04:14:25 +0100

    Smoother viridean city farming

--- farmer/victory_road_farmer.py
+++ farmer/victory_road_farmer.py
@@ -13,38 +13,38 @@ class VictoryRoadFarmer(Farmer):
                                                PROTrainerMove(["d", "1"], 10)])
     poke_center_move_set = PROTrainerMoveSequence([
         # Get out of the cave
-        PROTrainerMove(["d", "4"], 20),
-        PROTrainerMove(["w", "4"], 20),
+        PROTrainerMove(["d", "4"], 20, 0.75),
+        PROTrainerMove(["w", "4"], 20, 0.75),
         # Walk out
-        PROTrainerMove(["a", "4"], 1),
-        PROTrainerMove(["w", "4"], 1),
-        PROTrainerMove(["a", "4"], 1),
-        PROTrainerMove(["w", "4"], 1),
-        PROTrainerMove(["a", "4"], 1),
-        PROTrainerMove(["w", "4"], 1),
+        PROTrainerMove(["a", "4"], 1, 0.75),
+        PROTrainerMove(["w", "4"], 1, 0.75),
+        PROTrainerMove(["a", "4"], 1, 0.75),
+        PROTrainerMove(["w", "4"], 1, 0.75),
+        PROTrainerMove(["a", "4"], 1, 0.75),
+        PROTrainerMove(["w", "4"], 1, 0.75),
         # Jank out of the cave
-        PROTrainerMove(["d", "4"], 1),
-        PROTrainerMove(["w", "4"], 1),
-        PROTrainerMove(["d", "4"], 1),
-        PROTrainerMove(["w"], 1),
-        PROTrainerMove(["d"], 6),
+        PROTrainerMove(["d", "4"], 1, 0.75),
+        PROTrainerMove(["w", "4"], 1, 0.75),
+        PROTrainerMove(["d", "4"], 1, 0.75),
+        PROTrainerMove(["w"], 1, 0.75),
+        PROTrainerMove(["d"], 6, 0.75),
         # To Pokecenter
-        PROTrainerMove(["w"], 20),
-        PROTrainerMove(["d"], 9),
-        PROTrainerMove(["w"], 2),
+        PROTrainerMove(["w"], 20, 0.5),
+        PROTrainerMove(["d"], 9, 0.5),
+        PROTrainerMove(["w"], 2, 0.5),
         # In Pokecenter
-        PROTrainerMove(["w"], 4),
-        PROTrainerMove(["a"], 6),
+        PROTrainerMove(["w"], 4, 0.75),
+        PROTrainerMove(["a"], 6, 0.75),
         # Nurse Joy
-        PROTrainerMove(["w", " "], 10),
-        PROTrainerMove(["1", " ", "s"], 10),
+        PROTrainerMove(["w", " "], 10, 0.5),
+        PROTrainerMove(["1", " ", "s"], 10, 0.75),
         # Get out of the Pokecenter
-        PROTrainerMove(["d"], 10),
-        PROTrainerMove(["s"], 10),
-        PROTrainerMove(["a"], 12),
+        PROTrainerMove(["d"], 10, 0.5),
+        PROTrainerMove(["s"], 10, 0.5),
+        PROTrainerMove(["a"], 12, 0.5),
         # Into the cave
-        PROTrainerMove(["s", "1"], 20),
-        PROTrainerMove(["s", "1"], 20)
+        PROTrainerMove(["s", "1"], 20, 0.75),
+        PROTrainerMove(["s", "1"], 20, 0.75)
     ])
 
     # Init farm move sequence
