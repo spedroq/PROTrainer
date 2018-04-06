3b7a7ffd620eff18de53c91be1bfa2a5a7649d64
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2018-04-07 00:06:48 +0100

    Add fishing rod move set and turn off debug.

--- farmer/fishing_rod_farmer.py
+++ farmer/fishing_rod_farmer.py
@@ -12,11 +12,8 @@ class FishingRodFarmer(Farmer):
     WRAP_AROUND = 3
 
     # Define moves
-    default_move_set = PROTrainerMoveSequence([
-      PROTrainerMove(["1", "1"], 1)
-    ])
-    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4"], 25),
-                                                   PROTrainerMove(["w"], 25),
+    default_move_set = PROTrainerMoveSequence([PROTrainerMove(["1"], 1)])
+    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4", "w"], 25),
                                                    PROTrainerMove(["1", " ", "s"], 25),
                                                    PROTrainerMove(["s"], 25)])
 
