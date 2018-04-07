dc577230bc6a66dd35d712b29f31f5529fbdcd82
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2018-04-07 05:11:09 +0100

    Added the fishingrod configs.

--- farmer/fishing_rod_farmer.py
+++ farmer/fishing_rod_farmer.py
@@ -12,12 +12,10 @@ class FishingRodFarmer(Farmer):
     WRAP_AROUND = 3
 
     # Define moves
-    default_move_set = PROTrainerMoveSequence([PROTrainerMove(["a", "1"], 6),
-                                               PROTrainerMove(["d", "1"], 6)])
-    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4"], 25),
-                                                   PROTrainerMove(["w"], 25),
-                                                   PROTrainerMove(["1", " ", "s"], 25),
-                                                   PROTrainerMove(["s"], 25)])
+    default_move_set = PROTrainerMoveSequence([PROTrainerMove(["1"], 1, timeout=1)])
+    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4", "w"], 25, timeout=0.15),
+                                                   PROTrainerMove(["1", " ", "s"], 25, timeout=0.15),
+                                                   PROTrainerMove(["s"], 25, timeout=0.15)])
 
     # Init farm move sequence
     farm_move_sequence = default_move_set
