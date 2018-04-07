875732b2b4a090ec56ba9730e818044ed93024ae
Pedro Henrique de Oliveira Quintas <phoquintas@gmail.com>
2018-04-07 04:41:05 +0100

    Make the bot slower at farming, faster at going to the pokecenter.

--- farmer/fishing_rod_farmer.py
+++ farmer/fishing_rod_farmer.py
@@ -13,9 +13,9 @@ class FishingRodFarmer(Farmer):
 
     # Define moves
     default_move_set = PROTrainerMoveSequence([PROTrainerMove(["1"], 1, timeout=1)])
-    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4", "w"], 25),
-                                                   PROTrainerMove(["1", " ", "s"], 25),
-                                                   PROTrainerMove(["s"], 25)])
+    poke_center_move_set = PROTrainerMoveSequence([PROTrainerMove(["4", "w"], 25, timeout=0.15),
+                                                   PROTrainerMove(["1", " ", "s"], 25, timeout=0.15),
+                                                   PROTrainerMove(["s"], 25, timeout=0.15)])
 
     # Init farm move sequence
     #farm_move_sequence = default_move_set
